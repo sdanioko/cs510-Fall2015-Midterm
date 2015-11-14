1eb595ca312cecabf97915157afa2c0a468567dd
#Sidy Danioko# <65b4848fa0bd4e3faa1504a29ff600bf@compute.c.sage-math-inc.internal>
2015-11-14 04:49:33 +0000

    This is the revised version of test_attracor.py

--- test_attractor.py
+++ test_attractor.py
@@ -6,38 +6,51 @@ from attractor import Attractor
 
 
 def test_dt_value():
-    " Let's ensure that dt is calculated as it is supposed to be "
+    ''' Ensure that dt is calculated as it is supposed to be
+    '''
     a = Attractor()
-    assert a.end == a.dt * a.points
+    assert a.end == a.dt * a.points, "\n time step is not properly evaluated \n"
+
 def test_rhs_shape():
-    " Let's see how to validate the shape of rhs method return value "
+    ''' Validate that the shape of the rhs method return value
+    '''
     a = Attractor()
     xyz = np.array([0.0, 0.0, 0.0])
-    assert a.rhs(xyz).shape == (3, )
+    assert a.rhs(xyz).shape == (3, ), "\n the shape of the array that was returned is not 3 \n"
+
 def test_euler_shape():
-    " Let's see how to validate the shape of euler method return value"
+    ''' Validate that the shape of the euler method return value
+    '''
     a = Attractor()
     xyz = np.array([0.0, 0.0, 0.0])
-    assert a.euler(xyz).shape == (3, )
+    assert a.euler(xyz).shape == (3, ), "\n the shape of the array that was obtained from euler is not 3 \n"
+
 def test_rk2_shape():
-    " Let's see how to validate the shape of rk2 method return value "
+    ''' Validate that the shape of rk2 method return value
+    '''
     a = Attractor()
     xyz = np.array([0.0, 0.0, 0.0])
-    assert a.rk2(xyz).shape == (3, )
+    assert a.rk2(xyz).shape == (3, ), "\n the shape of the array that was obtained from rk2 is not 3 \n"
+
 def test_rk4_shape():
-    " Let's see how to validate the shape of rk4 method return value "
+    ''' Validate that the shape of rk4 method return value
+    '''
     a = Attractor()
     xyz = np.array([0.0, 0.0, 0.0])
-    assert a.rk4(xyz).shape == (3, )
+    assert a.rk4(xyz).shape == (3, ), "\n the shape of the array that was obtained from rk4 is not 3 \n"
+
 def test_evolve_shape():
-    " Let's how to validate the shape of evolve method return value " 
+    ''' Validate that the shape of evolve method return value
+    '''
     a = Attractor()
-    assert a.evolve().shape == (a.points, 4)
+    assert a.evolve().shape == (a.points, 4), "\n the array that was obtained does not have a proper shape \n"
 def test_save_csv():
-    " Let's ensure that csv file is created "
-    filename = 'test_cvs_filename.csv'
+    ''' Ensure that the csv file is created
+    '''
+    filename = 'filename.csv'
     a = Attractor()
     os.remove(filename)
     a.evolve()
     a.save(filename)
-    assert os.path.exists(filename)
+    assert os.path.exists(filename), "\n no output file found \n"
+    print(" test_save_csv method is working as expected ")
